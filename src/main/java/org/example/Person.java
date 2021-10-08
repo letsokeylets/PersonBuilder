d7c69965a93a9f50a157cffe2c91b9cf2041da5f
Mozdir Taras <letsokey@mail.ru>
2021-10-08 12:42:42 +0400

    Initial commit

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,55 @@
+package org.example;
+
+public class Person {
+    protected final String name;
+    protected final String surname;
+    protected int age;
+    protected String address;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public boolean hasAge() {
+        return age == 0 ? false : true;
+    }
+
+    public void happyBirthday() {
+        age++;
+    }
+
+    public boolean hasAddress() {
+        return address == null ? false : true;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
