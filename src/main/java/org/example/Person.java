89cfafb45b30310e9a160e034039d8ec14f967c7
Mozdir Taras <letsokey@mail.ru>
2021-10-08 14:04:15 +0400

    Builder Created

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Objects;
+
 public class Person {
     protected final String name;
     protected final String surname;
@@ -17,8 +19,15 @@ public class Person {
         this.age = age;
     }
 
+    public Person(String name, String surname, int age, String address) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.address = address;
+    }
+
     public boolean hasAge() {
-        return age == 0 ? false : true;
+        return age != 0;
     }
 
     public void happyBirthday() {
@@ -26,7 +35,7 @@ public class Person {
     }
 
     public boolean hasAddress() {
-        return address == null ? false : true;
+        return address != null;
     }
 
     public String getName() {
@@ -42,6 +51,7 @@ public class Person {
     }
 
     public void setAge(int age) {
+        if (!hasAge())
         this.age = age;
     }
 
@@ -51,5 +61,34 @@ public class Person {
 
     public void setAddress(String address) {
         this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "имя='" + name + '\'' +
+                ", фамилия='" + surname + '\'' +
+                ", возраст= " + age +
+                ", адресс'" + address + '\'';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(name, person.name) && Objects.equals(surname, person.surname) && Objects.equals(address, person.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, age, address);
+    }
+
+    public PersonBuilder newChildBuilder() {
+        return new PersonBuilder()
+                .setSurname(surname)
+                //Поставил 1, так как по задаче нет понимания какой именно возраст необходимо установить
+                .setAge(1)
+                .setAddress(address);
     }
 }
--- src/main/java/org/example/PersonBuilder.java
+++ src/main/java/org/example/PersonBuilder.java
@@ -1,4 +1,43 @@
 package org.example;
 
 public class PersonBuilder {
+    protected String name;
+    protected String surnameme;
+    protected int age;
+    protected String address;
+
+    public PersonBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public PersonBuilder setSurname(String surname) {
+        this.surnameme = surname;
+        return this;
+    }
+
+    public PersonBuilder setAge(int age) {
+        this.age = age;
+        return this;
+    }
+
+    public PersonBuilder setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+
+    public Person build() {
+        if (name == null || surnameme == null) {
+            throw new IllegalStateException("Не хватает обязательных полей");
+        }
+        if (age < 0 || age > 150) {
+            throw new IllegalArgumentException("Введены недопустимые значения возраста");
+        }
+        Person person = new Person(name, surnameme, age);
+        if (address != null) {
+            person.setAddress(address);
+        }
+        return person;
+    }
 }
